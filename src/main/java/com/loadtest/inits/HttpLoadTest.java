867bc25b89c267b832653a1d36832a798120c87d
System Administrator <root@sumeets-MacBook-Pro.local>
2017-01-14 22:11:52 +0530

    Basic Structure and First Commit :)

---
+++ src/main/java/com/loadtest/inits/HttpLoadTest.java
@@ -0,0 +1,9 @@
+package com.loadtest.inits;
+
+/**
+ * @author sumeetpatil
+ * Load Test Init for http protocol
+ */
+public class HttpLoadTest {
+
+}
---
+++ src/main/java/com/loadtest/inits/Init.java
@@ -0,0 +1,13 @@
+package com.loadtest.inits;
+
+/**
+ * @author sumeetpatil
+ * Load Test Init
+ */
+public class Init {
+
+	public static void main(String[] args) {
+
+	}
+
+}
